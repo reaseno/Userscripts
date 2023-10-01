1cbbc0b02d54366a6626ad947e7aa3625c62af26
reseano <>
2023-10-01 14:23:57 +0200

    fixed key event bug

--- IMDb-skip-mediaviewer-ads.js
+++ IMDb-skip-mediaviewer-ads.js
@@ -2,12 +2,12 @@
 // @name           IMDb: skip media viewer advertisements
 // @version        20231001
 // @author         reaseno
-// @description    Skips advertisments in the media viewer.
+// @description    Skips advertisements in the media viewer.
 // @icon           http://imdb.com/favicon.ico
 // @homepage       https://greasyfork.org/en/scripts/476492
 // @namespace      https://greasyfork.org/users/781194
 // @match          *://www.imdb.com/*/mediaviewer/*
-// @run-at         document-start
+// @grant          GM_addStyle
 // @compatible     chrome
 // @license        GPL3
 // @noframes
@@ -15,49 +15,69 @@
 
 "use strict";
 
-// Initialize variables to track the previous action
+GM_addStyle(`
+    .sc-857a727e-10.bLFLMI {
+        display: none;
+    }
+`);
+
+// to track the previous action
 let previousAction = null; // Values: "previous", "next", or null (initial state)
 const previousButton = document.querySelector("div.media-viewer__page-left");
 const nextButton = document.querySelector("div.media-viewer__page-right");
 
-// Function to perform the navigation action
-function skipAdvertisement() {
+// Function to perform the advertisement action
+function handleAdvertisement() {
     const actionBar = document.querySelector("div[data-testid=action-bar]");
 
+    // if advertisement
     if (actionBar && !/\d/.test(actionBar.innerText)) {
         console.log("Advertisement detected");
-        console.log(previousAction);
 
-        if (previousAction === "previous" && previousButton) {
-            previousButton.click();
-        } else if (previousAction === "next" && nextButton) {
-            nextButton.click();
+        if (previousAction === "previous") {
+            // next/prev button not clickable if not visible
+            const event = new KeyboardEvent("keydown", {
+                key: "ArrowLeft",
+                keyCode: 37,
+                which: 37,
+                bubbles: true,
+            });
+            document.dispatchEvent(event);
+        } else if (previousAction === "next") {
+            // next/prev button not clickable if not visible
+            const event = new KeyboardEvent("keydown", {
+                key: "ArrowRight",
+                keyCode: 39,
+                which: 39,
+                bubbles: true,
+            });
+            document.dispatchEvent(event);
         }
     }
 }
 
 // Create a MutationObserver to watch for changes in the action bar
-const observer = new MutationObserver(function (mutationsList) {
-    for (let mutation of mutationsList) {
-        if (mutation.type === "childList" || mutation.type === "characterData") {
-            // Call the navigateMediaViewer function when the action bar changes
-            skipAdvertisement();
-        }
-    }
+const observer = new MutationObserver(() => {
+    // ensures that keyboard event is handled first, otherwise wrong direction when changing direction over an ad
+    setTimeout(() => {
+        handleAdvertisement();
+    }, 1);
 });
 
 // Start observing changes in the action bar
 const actionBar = document.querySelector("div[data-testid=action-bar]");
 if (actionBar) {
-    observer.observe(actionBar, { attributes: true, childList: true, characterData: true });
+    observer.observe(actionBar, { childList: true, characterData: true });
 }
 
 // Listen for keydown events to track cursor direction
 window.addEventListener("keydown", function (event) {
     if (event.key === "ArrowLeft") {
         previousAction = "previous";
+        // console.log(`event: ${previousAction}`);
     } else if (event.key === "ArrowRight") {
         previousAction = "next";
+        // console.log(`event: ${previousAction}`);
     }
 });
 
@@ -69,6 +89,3 @@ previousButton?.addEventListener("click", function () {
 nextButton?.addEventListener("click", function () {
     previousAction = "next";
 });
-
-// Call navigateMediaViewer initially (in case the page is already in the desired state)
-skipAdvertisement();
