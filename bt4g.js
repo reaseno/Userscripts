b2ab2cef35423cb06432cee9220d4593793e506e
oxFilla <mikkel.filla@oxid-esales.com>
2023-10-29 20:52:48 +0100

    infinite scroll support

--- bt4g.js
+++ bt4g.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           BT4G Add Magnet Links
-// @version        20230702
+// @version        20231029
 // @author         reaseno
 // @description    Adds magnet links to the search list of BT4G
 // @icon           http://bt4gprx.com/favicon.ico
@@ -23,7 +23,7 @@ function createMagnetLink(link, hash) {
 
     const imgElement = document.createElement("img");
     imgElement.setAttribute("src", "https://cdn.jsdelivr.net/gh/zxf10608/JavaScript/icon/magnet00.png");
-    imgElement.setAttribute("class", "magnet");
+    imgElement.setAttribute("class", "magnet-link");
     imgElement.setAttribute("target", "_self");
     imgElement.setAttribute(
         "style",
@@ -40,43 +40,79 @@ function getHashFromHref(href) {
     return matches ? matches[2] : null;
 }
 
+function addMagnetInSearch() {
+    const links = document.querySelectorAll('a[href*="/magnet/"]:not([href^="magnet:"])');
+    const promises = [];
+
+    links.forEach((link) => {
+        // skip if magnet link exists
+        const magnetLink = link.getAttribute("data-magnet-added");
+        if (magnetLink === "true") {
+            return;
+        }
+
+        promises.push(
+            fetch(link.href)
+                .then((response) => response.text())
+                .then((html) => {
+                    const parser = new DOMParser();
+                    const doc = parser.parseFromString(html, "text/html");
+                    const downloadLink = doc.querySelector('a[href^="//downloadtorrentfile.com/hash/"]');
+                    if (downloadLink) {
+                        const hash = getHashFromHref(downloadLink.href.split("/").pop().split("?")[0]);
+                        if (hash) {
+                            createMagnetLink(link, hash);
+                            link.setAttribute("data-magnet-added", "true");
+                        }
+                    }
+                })
+                .catch((error) => {
+                    console.error("Error getting magnet link:", error);
+                })
+        );
+    });
+
+    return Promise.all(promises).then(() => {
+        // remove spam elements
+        setTimeout(() => {
+            // first element is not relevant and creates an error
+            const mainObjects = [].slice.call(document.querySelectorAll("body > main > div > div:nth-child(4) > div > div"), 1);
+
+            for (let mainObject of mainObjects) {
+                if (mainObject.childNodes[1].innerText.match("Downloader.exe")) {
+                    mainObject.style = "display:none";
+                }
+            }
+        }, 100);
+    });
+}
+
+function searchObserver() {
+    console.log("searchObserver");
+    let observer = new MutationObserver(() => {
+        observer.disconnect();
+        setTimeout(() => {
+            addMagnetInSearch().then(() => {
+                searchObserver();
+            });
+        }, 500);
+    });
+
+    // start observing
+    observer.observe(document.body, {
+        childList: true,
+        subtree: true,
+    });
+}
+
 // ----------------------------------------------------------------------------------------------------
 // Main
 // ----------------------------------------------------------------------------------------------------
 
 // search page
 if (window.location.href.match(/\/search/)) {
-    const links = document.querySelectorAll('a[href*="/magnet/"]:not([href^="magnet:"])');
-    links.forEach((link) => {
-        fetch(link.href)
-            .then((response) => response.text())
-            .then((html) => {
-                const parser = new DOMParser();
-                const doc = parser.parseFromString(html, "text/html");
-                const downloadLink = doc.querySelector('a[href^="//downloadtorrentfile.com/hash/"]');
-                if (downloadLink) {
-                    const hash = getHashFromHref(downloadLink.href.split("/").pop().split("?")[0]);
-                    if (hash) {
-                        createMagnetLink(link, hash);
-                    }
-                }
-            })
-            .catch((error) => {
-                console.error("Error getting magnet link:", error);
-            });
-    });
-
-    // remove spam elements
-    setTimeout(() => {
-        // first element is not relevant and creates an error
-        const mainObjects = [].slice.call(document.querySelectorAll("body > main > div > div:nth-child(4) > div > div"), 1);
-
-        for (let mainObject of mainObjects) {
-            if (mainObject.childNodes[1].innerText.match("Downloader.exe")) {
-                mainObject.style = "display:none";
-            }
-        }
-    }, 100);
+    addMagnetInSearch();
+    searchObserver();
 }
 
 // details page
